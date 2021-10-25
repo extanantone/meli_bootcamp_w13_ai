b29ee8d75f53bf07558e597c59fd10c32f4e76d2
Federico Zito <federico_zito@mercadolibre.com>
2021-10-25 14:38:34 -0300

    Ejercicio 1, Aumento

---
+++ Java/Clase 1/src/Pra.java
@@ -0,0 +1,26 @@
+public class Pra {
+
+    String dni = "12345678"; // dni de ejemplo
+    double sueldoBase = 46000; // monto de ejemplo
+    double sueldoConAumento;
+
+    public void calcularSueldo() {
+
+        // Tu codigo aqui
+        if (sueldoBase <= 20000) {
+            sueldoConAumento = (sueldoBase / 100) * 20 + sueldoBase;
+        } else {
+            if (sueldoBase > 20000 && sueldoBase <= 45000) {
+                sueldoConAumento = (sueldoBase / 100) * 10 + sueldoBase;
+            } else {
+                sueldoConAumento = (sueldoBase / 100) * 5 + sueldoBase;
+            }
+        }
+
+        System.out.println("El nuevo sueldo del empleado es de: " + sueldoConAumento);
+    }
+    public static void main(String[] args) {
+        new Pra().calcularSueldo();
+    }
+    
+}
