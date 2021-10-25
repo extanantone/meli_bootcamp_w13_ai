f4ae6d46e93dc8e8fef1766b97e9a35ed66ebf1c
dagomeli <diego.guerra@mercadolibre.com.co>
2021-10-25 12:44:19 -0500

    Ejercicio de sueldos y facturas

---
+++ Java/Java-EstructructurasDeControl1/src/com/company/ejercicioFacturas.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ejercicioFacturas {
+
+
+        List<String> mensajeStrings = new LinkedList<>();
+
+        int[] serviciosCli = {1,1,2,2,2,1,2}; //vector de 7 posiciones con tipos de servicios
+        double totalFactura;
+
+        public void calcularMontoFactura() {
+            //Tu codigo aqui
+            for (int i = 0; i < serviciosCli.length ; i++) {
+                if (serviciosCli[i] == 1) {
+                    imprimirMensaje("El tipo de servicio es: " + serviciosCli[i]);
+                    totalFactura = 1500;
+                    imprimirMensaje ("El monto de la factura es de: " + totalFactura);
+                }
+                else {
+                    imprimirMensaje("El tipo de servicio es: " + serviciosCli[i]);
+                    totalFactura = 2200;
+                    imprimirMensaje("El monto de la factura es de: " + totalFactura);
+                }
+            }
+        }
+
+        private void imprimirMensaje(String mensaje) {
+            mensajeStrings.add(mensaje);
+            System.out.println(mensaje);
+        }
+
+        public static void main(String[] args) {
+            new ejercicioFacturas().calcularMontoFactura();
+        }
+
+
+
+}
---
+++ Java/Java-EstructructurasDeControl1/src/com/company/ejercicioSueldos.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class ejercicioSueldos {
+
+
+
+        String dni = "12345678"; // dni de ejemplo
+        double sueldoBase = 19000; // monto de ejemplo
+        double sueldoConAumento;
+
+        public void calcularAumento() {
+
+            // Tu codigo aqui
+            if (sueldoBase <= 20000) {
+                sueldoConAumento = sueldoBase + sueldoBase*0.2;
+            }
+            else {
+                if (sueldoBase > 20000 && sueldoBase <= 45000){
+                    sueldoConAumento = sueldoBase + (sueldoBase*0.1);
+                }
+                else {
+                    sueldoConAumento = sueldoBase + sueldoBase*0.05;
+                }
+            }
+
+            System.out.println ("El nuevo sueldo del empleado es de: " + sueldoConAumento);
+        }
+
+        public static void main(String[] args) {
+            new ejercicioSueldos().calcularAumento();
+        }
+
+
+}
