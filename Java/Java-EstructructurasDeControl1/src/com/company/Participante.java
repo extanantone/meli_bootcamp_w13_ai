1c6f9cd13b391845905b3a4908be4fed7a460d95
dagomeli <diego.guerra@mercadolibre.com.co>
2021-10-25 17:16:41 -0500

    Ejercicio carrera selva

---
+++ Java/Java-EstructructurasDeControl1/src/com/company/Participante.java
@@ -0,0 +1,87 @@
+package com.company;
+
+public class Participante {
+
+    private int dni;
+    private String nombre;
+    private String apellido;
+    private int nroEmergencia;
+    private String grupoSanguineo;
+    private int celular;
+    private int edad;
+    private int aporte;
+
+    public Participante(int dni, String nombre, String apellido, int nroEmergencia, String grupoSanguineo, int celular, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nroEmergencia = nroEmergencia;
+        this.grupoSanguineo = grupoSanguineo;
+        this.celular = celular;
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public int getAporte() {
+        return aporte;
+    }
+
+    public void setAporte(int aporte) {
+        this.aporte = aporte;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNroEmergencia() {
+        return nroEmergencia;
+    }
+
+    public void setNroEmergencia(int nroEmergencia) {
+        this.nroEmergencia = nroEmergencia;
+    }
+
+    public String getGrupoSanguineo() {
+        return grupoSanguineo;
+    }
+
+    public void setGrupoSanguineo(String grupoSanguineo) {
+        this.grupoSanguineo = grupoSanguineo;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ Java/Java-EstructructurasDeControl1/src/com/company/ejercicioCarreraSelva.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ejercicioCarreraSelva {
+
+    List<Participante> listaParticipantes = new ArrayList<>();
+    HashMap<Integer, Participante> circuitoChico = new HashMap<>();
+    HashMap<Integer, Participante> circuitoMedio = new HashMap<>();
+    HashMap<Integer, Participante> circuitoAvanzado = new HashMap<>();
+
+    Participante juan = new Participante(156, "Juan", "Perez", 3338490, "O+", 32193193, 21);
+    Participante marcos = new Participante(256, "Marcos", "Lopez", 3338491, "O+", 32193193, 14);
+    Participante carlos = new Participante(356, "Carlos", "Alvarez", 3338492, "O+", 32193193, 15);
+    Participante maria = new Participante(456, "Maria", "Granjas", 3138491, "O+", 32193193, 24);
+    Participante carla = new Participante(556, "Carla", "Moreno", 3328591, "O+", 32193193, 31);
+
+    public void inscribirParticipantes(){
+        listaParticipantes.add(juan);
+        listaParticipantes.add(marcos);
+        listaParticipantes.add(carlos);
+        listaParticipantes.add(maria);
+        listaParticipantes.add(carla);
+
+        int aporte;
+        int menor;
+        int circuitoAleatorio;
+
+        for (Participante p: listaParticipantes) {
+            aporte = 0;
+            menor = p.getEdad() < 18 ? 2:3;
+            circuitoAleatorio = 0;
+            circuitoAleatorio = (int)(Math.random()*menor) + 1;
+
+            if (circuitoAleatorio == 1){
+                int participante = circuitoChico.size();
+                aporte = menor==2 ? 1300:1500;
+                p.setAporte(aporte);
+                circuitoChico.put(participante+1,p);
+            }
+            else if(circuitoAleatorio == 2){
+                int participante = circuitoMedio.size();
+                aporte = menor == 2 ? 2000:2300;
+                p.setAporte(aporte);
+                circuitoMedio.put(participante+1,p);
+            }
+            else{
+                int participante = circuitoAvanzado.size();
+                p.setAporte(2800);
+                circuitoAvanzado.put(participante+1,p);
+            }
+        }
+
+        System.out.println("Lista de Inscritos a circuitos");
+        System.out.println("------------------------------");
+        System.out.println("Circuito Chico");
+        for (Map.Entry<Integer, Participante> p: circuitoChico.entrySet()) {
+            System.out.println("Participante "+p.getKey()+": Nombre - "+p.getValue().getNombre()+" Edad: "+p.getValue().getEdad()+" Aporte: "+p.getValue().getAporte());
+        }
+        System.out.println();
+        System.out.println("Circuito Medio");
+        for (Map.Entry<Integer, Participante> p: circuitoMedio.entrySet()) {
+            System.out.println("Participante "+p.getKey()+": Nombre - "+p.getValue().getNombre()+" Edad: "+p.getValue().getEdad()+" Aporte: "+p.getValue().getAporte());
+        }
+        System.out.println();
+        System.out.println("Circuito Avanzado");
+        for (Map.Entry<Integer, Participante> p: circuitoAvanzado.entrySet()) {
+            System.out.println("Participante "+p.getKey()+": Nombre - "+p.getValue().getNombre()+" Edad: "+p.getValue().getEdad()+" Aporte: "+p.getValue().getAporte());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new ejercicioCarreraSelva().inscribirParticipantes();
+    }
+
+}
