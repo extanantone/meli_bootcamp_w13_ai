f5149ab86ce3bbe927f1ac0a81b615989900f245
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-10-26 09:22:09 -0500

    ejercicio 1 poo

--- Java/poo/src/Persona.java
+++ Java/poo/src/Persona.java
@@ -48,4 +48,44 @@ public class Persona {
                 ", altura=" + altura +
                 '}';
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 }
--- Java/poo/src/mainPoo.java
+++ Java/poo/src/mainPoo.java
@@ -3,6 +3,10 @@ public class mainPoo {
         Persona persona1 = new Persona();
         Persona persona2 = new Persona("diana",22,"1234");
         Persona persona3 = new Persona("camilo", 25, "1456", 90, 1.70);
+        System.out.println("Nombre:"+ persona3.getNombre());
+        System.out.println("Edad:"+persona3.getEdad());
+        System.out.println("Dni:"+persona3.getDni());
+        System.out.println("Peso:"+persona3.getPeso());
         int imc=persona3.calcularIMC();
         if(imc==-1){
             System.out.println("El IMC de la persona es menor a 20 es decir esta bajo de peso");
@@ -11,6 +15,7 @@ public class mainPoo {
         }else{
             System.out.println("El IMC de la persona es mayor a 25, es decir que esta en sobrepeso");
         }
+        System.out.println("Edad:"+persona3.getEdad());
         boolean mayor = persona3.esMayorDeEdad();
         if(mayor){
             System.out.println("La persona es mayor de edad");
