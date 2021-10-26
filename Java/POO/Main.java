fb53950c709f413272cdb2969cdc27e24c60fe33
jessica-gomez <jessica.gomez@mercadolibre.com>
2021-10-26 13:33:56 -0300

    ejercicio IMC - POO

---
+++ Java/POO/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona personaSinParametros = new Persona();
+        Persona personaDniNombreEdad = new Persona("Armando", 48, "17556702");
+        Persona personaTodosLosParametros = new Persona ("Puentes", 33, "33366779", 78, 1.83);
+
+        // Persona personaConError = new Persona("Marcos", "33");
+
+
+        String IMC = personaTodosLosParametros.calcularIMC();
+        System.out.println("Tu IMC es " + IMC);
+
+        Boolean esMayor = personaTodosLosParametros.esMayorDeEdad();
+
+        if(esMayor)
+            System.out.println("La persona es mayor de edad");
+        else
+            System.out.println("La persona no es mayor de edad");
+
+        System.out.println(personaTodosLosParametros.toString());
+
+    }
+}
---
+++ Java/POO/Persona.java
@@ -0,0 +1,43 @@
+public class Persona{
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularIMC(){
+        double IMC = peso / (altura * altura);
+        return (IMC < 20) ? "Bajo peso" : (IMC > 25) ? "Sobrepeso" : "Saludable";
+    }
+    public Boolean esMayorDeEdad(){
+        return this.edad  >= 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+}
