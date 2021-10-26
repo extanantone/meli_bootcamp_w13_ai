04867b80dbe2999c634c3ddb30ad0dc504d7ffa0
Lautaro Goyenechea <lautaro.goyenechea@mercadolibre.com>
2021-10-26 16:37:16 -0300

    Resolucion Ejercicio 4 - Encapsulamiento, Herencia y Polimorfismo

---
+++ Java/ejercicio4/src/Distribuidora.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Distribuidora {
+
+    private static List<Producto> productos = new ArrayList<>(
+            Arrays.asList(
+                    new Perecedero("Leche", 67.8, 3),
+                    new NoPerecedero("Arroz", 31.5, "Cereal"),
+                    new Perecedero("Manteca", 120, 1),
+                    new NoPerecedero("Fideos", 27, "Pastas"),
+                    new Perecedero("Queso Untable", 115, 2)
+            )
+    );
+
+    static double calcularPrecio(List<Producto> productos) {
+        return productos.stream()
+                .map(producto -> producto.calcular(5))
+                .mapToDouble(Double::doubleValue)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("La venta da un total de %.2f", calcularPrecio(productos));
+    }
+}
---
+++ Java/ejercicio4/src/Perecedero.java
@@ -0,0 +1,32 @@
+public class Perecedero extends Producto {
+    private int diasPorCalcular;
+
+    public Perecedero(String nombre, double precio, int diasPorCalcular) {
+        super(nombre, precio);
+        this.diasPorCalcular = diasPorCalcular;
+    }
+
+    public int getDiasPorCalcular() {
+        return diasPorCalcular;
+    }
+
+    public void setDiasPorCalcular(int diasPorCalcular) {
+        this.diasPorCalcular = diasPorCalcular;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecedero {" +
+                "diasPorCalcular=" + diasPorCalcular +
+                '}';
+    }
+
+    @Override
+    public double calcular(int cantidadDeProductos) {
+        double precio = super.calcular(cantidadDeProductos);
+        if (diasPorCalcular == 1) return precio/4;
+        if (diasPorCalcular == 2) return precio/3;
+        if (diasPorCalcular == 3) return precio/2;
+        return precio;
+    }
+}
