d71db636ce173316f553bfaa2e57a2693cb1c4e3
Angelica Landazabal <angelica.landazabal@mercadolibre.com.co>
2021-10-27 18:41:33 -0500

    Ingresando datos del participante por teclado

--- Java/C1P2_CarreraSelva/src/Main.java
+++ Java/C1P2_CarreraSelva/src/Main.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
+        Scanner entradaEscaner = new Scanner(System.in);
+
         List<Competidor> cChico = new ArrayList<Competidor>();
         List<Competidor> cMedio = new ArrayList<Competidor>();
         List<Competidor> cAvanzado = new ArrayList<Competidor>();
@@ -12,76 +15,94 @@ public class Main {
         Competidor comp1 = new Competidor(1234,"Ana","Lopez",19,1234567,9876543,"O+");
         cAvanzado.add(comp1);
 
+        System.out.println("Ingrese DNI: ");
+        int dni = entradaEscaner.nextInt();
+        System.out.println("Ingrese nombre: ");
+        String nombre = entradaEscaner.next();
+        System.out.println("Ingrese apellido: ");
+        String apellido = entradaEscaner.next();
+        System.out.println("Ingrese edad: ");
+        int edad = entradaEscaner.nextInt();
+        System.out.println("Ingrese celular: ");
+        int celular = entradaEscaner.nextInt();
+        System.out.println("Ingrese número de emergencia: ");
+        int numEmergencia = entradaEscaner.nextInt();
+        System.out.println("Ingrese grupo sanguieo: ");
+        String gsanguineo = entradaEscaner.next();
+
+        Competidor comp = new Competidor(dni,nombre,apellido,edad,celular,numEmergencia,gsanguineo);
+
         for( int i = 0; i < cChico.size(); i++){
-            if( cChico.get(i).getDni() == comp1.getDni() ){
+            if( cChico.get(i).getDni() == comp.getDni() ){
                 System.out.println("El participante con DNI " +
-                        comp1.getDni() +
+                        comp.getDni() +
                         " ya se encuentra inscrito en el circuito chico.");
             }
 
         }
 
         for( int i = 0; i < cMedio.size(); i++){
-            if( cMedio.get(i).getDni() == comp1.getDni() ){
+            if( cMedio.get(i).getDni() == comp.getDni() ){
                 System.out.println("El participante con DNI " +
-                        comp1.getDni() +
+                        comp.getDni() +
                         " ya se encuentra inscrito en el circuito medio.");
             }
 
         }
 
         for( int i = 0; i < cAvanzado.size(); i++){
-            if( cAvanzado.get(i).getDni() == comp1.getDni() ){
+            if( cAvanzado.get(i).getDni() == comp.getDni() ){
                 System.out.println("El participante con DNI " +
-                        comp1.getDni() +
+                        comp.getDni() +
                         " ya se encuentra inscrito en el circuito avanzado.");
             }
 
         }
 
-        int tipoCircuito = 3;
+        System.out.println("Ingrese tipo circuito: 1-Chico; 2-Medio; 3-Avanzado ->");
+        int tipoCircuito = entradaEscaner.nextInt();
         int numCompetidor = 0;
         switch (tipoCircuito){
 
             case 1:
                 numCompetidor = cChico.size() + 1;
-                comp1.setNumParticipante(numCompetidor);
+                comp.setNumParticipante(numCompetidor);
 
-                if( comp1.getEdad() < 18 ){
-                    comp1.setPagoInscripcion(1300);
+                if( comp.getEdad() < 18 ){
+                    comp.setPagoInscripcion(1300);
                 }else{
-                    comp1.setPagoInscripcion(1500);
+                    comp.setPagoInscripcion(1500);
                 }
 
-                cChico.add(comp1);
+                cChico.add(comp);
                 System.out.println("Competidor inscrito en Circuito Chico con número " +
                         cChico.get(numCompetidor-1).getNumParticipante());
                 System.out.println("El participante debe pagar: $" + cChico.get(numCompetidor-1).getPagoInscripcion());
                 break;
             case 2:
                 numCompetidor = cMedio.size() + 1;
-                comp1.setNumParticipante(numCompetidor);
+                comp.setNumParticipante(numCompetidor);
 
-                if( comp1.getEdad() < 18 ){
-                    comp1.setPagoInscripcion(2000);
+                if( comp.getEdad() < 18 ){
+                    comp.setPagoInscripcion(2000);
                 }else{
-                    comp1.setPagoInscripcion(2300);
+                    comp.setPagoInscripcion(2300);
                 }
 
-                cMedio.add(comp1);
+                cMedio.add(comp);
                 System.out.println("Competidor inscrito en Circuito Médio con número " +
                         cMedio.get(numCompetidor-1).getNumParticipante());
                 System.out.println("El participante debe pagar: $" + cMedio.get(numCompetidor-1).getPagoInscripcion());
                 break;
             case 3:
                 numCompetidor = cAvanzado.size() + 1;
-                comp1.setNumParticipante(numCompetidor);
+                comp.setNumParticipante(numCompetidor);
 
-                if( comp1.getEdad() < 18 ){
+                if( comp.getEdad() < 18 ){
                     System.out.println("El participante no puede competir por ser menor de edad");
                 }else{
-                    comp1.setPagoInscripcion(2800);
-                    cAvanzado.add(comp1);
+                    comp.setPagoInscripcion(2800);
+                    cAvanzado.add(comp);
                     System.out.println("Competidor inscrito en Circuito Avanzado con número " +
                             cAvanzado.get(numCompetidor-1).getNumParticipante());
                     System.out.println("El participante debe pagar: $" + cAvanzado.get(numCompetidor-1).getPagoInscripcion());
