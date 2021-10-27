e8c5afd184282b74f38065d315be6230922a6038
irarevalo <ivan.arevalo@mercadolibre.com.co>
2021-10-27 13:14:43 -0500

    ejercicio excepciones y polimorfismo

---
+++ Java/ejercicio_polimorfismo_herencia/src/Perecedero.java
@@ -0,0 +1,36 @@
+public class Perecedero extends Producto {
+    private int diasPorCaducar;
+
+    public Perecedero(int diasPorCaducar, String nombre, double precio) {
+        this.diasPorCaducar = diasPorCaducar;
+        super.setNombre(nombre);
+        super.setPrecio(precio);
+    }
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Perecedero{" +
+                "diasPorCaducar=" + diasPorCaducar +
+                '}';
+    }
+    @Override
+    public double calcular(int cantidadDeProductos){
+        double resultado = super.getPrecio() * cantidadDeProductos;
+        if(diasPorCaducar==1){
+            resultado/=4;
+        } else if (diasPorCaducar==2){
+            resultado/=3;
+        } else if(diasPorCaducar==3){
+            resultado/=2;
+        }
+        return resultado;
+    }
+}
---
+++ Java/ejercicio_polimorfismo_herencia/src/PracticaExcepciones.java
@@ -0,0 +1,12 @@
+public class PracticaExcepciones {
+    private int a = 0;
+    private int b = 300;
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
