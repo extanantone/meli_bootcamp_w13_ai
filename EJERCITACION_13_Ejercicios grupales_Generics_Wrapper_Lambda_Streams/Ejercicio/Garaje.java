7e36fb7e6a8ad91a2e7ac0e1fd2584543adede41
aferrandoMELI <andres.ferrando@mercadolibre.com>
2021-10-27 19:51:57 -0300

    Ejercicio Streams

---
+++ EJERCITACION_13_Ejercicios grupales_Generics_Wrapper_Lambda_Streams/Ejercicio/Garaje.java
@@ -0,0 +1,31 @@
+package Ejercicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garaje {
+
+    private long id;
+    private List<Vehiculo> vehiculos = new ArrayList<>();
+
+    public Garaje(long id, List<Vehiculo> vehiculos) {
+        this.id = id;
+        this.vehiculos = vehiculos;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+}
