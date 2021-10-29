d47ec963506f3bd3da5a8b4413d96418850af765
vagarro <vanesa.garro@mercadolibre.com>
2021-10-29 17:03:41 -0300

    Ejercicio spring de Numeros Romanos y ejercicio Spring de codigo morse

--- Spring/CodigoMorse/src/main/java/com/Meli/CodigoMorse/Service/MorseService.java
+++ Spring/CodigoMorse/src/main/java/com/Meli/CodigoMorse/Service/MorseService.java
@@ -5,28 +5,14 @@ import com.Meli.CodigoMorse.Entity.Morse;
 public class MorseService {
 
     public static String getTexto(String palabraMorse){
-        Morse mors = new Morse();
-        String morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..",
-                "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",
-                "|", ".---", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----"};
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890";
-
+        Morse morse = new Morse();
         StringBuilder salida = new StringBuilder();
 
         String palabrasDivididas[] = palabraMorse.split("   ");
         for (String pd : palabrasDivididas) {
             String letrasDivididas[] = pd.split(" ");
             for (String ld : letrasDivididas) {
-                salida.append(alphabet.charAt(j));
-
-
-                /*for (int j = 0; j < morse.length; j++) {
-                    System.out.println(morse[j].equals(ld));
-                    if (morse[j].equals(ld)) {
-                        salida.append(alphabet.charAt(j));
-                    }
-                }*/
-
+                salida.append(morse.getLetra(ld));
             }
             salida.append(" ");
         }
