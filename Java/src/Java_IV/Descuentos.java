bd4c375a1bea332f725e696565e5a2158d1cbab9
Francisco Balboa <francisco.balboa@mercadolibre.com>
2021-10-29 18:50:34 -0300

    Spring I

--- Java/src/Java_IV/Descuentos.java
+++ Java/src/Java_IV/Descuentos.java
@@ -1,6 +1,8 @@
 package Java_IV;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Descuentos {
     private RepositorioLocalizadores repoLocalizadores;
@@ -14,14 +16,18 @@ public class Descuentos {
         return montoFinal;
     }
 
-    private int descuentoPorLocalizadores(double monto, Cliente cliente){
+    private int descuentoPorLocalizadores(Cliente cliente){
         return repoLocalizadores.getListaLocalizador().stream()
                 .reduce(0,(acc,cv)-> cv.getCliente().equals(cliente)? acc+1:acc , Integer::sum)
                 > 1 ? 10 : 0;
     }
 
-    private int descuentoPorPaquete(double monto, ArrayList<Reserva> paquete){
-        return 0;
+    private int descuentoPorPaquete(ArrayList<Reserva> paquete){
+        List<TipoReserva> listaPaqueteCompleto = Arrays.asList(TipoReserva.values());
+        for (Reserva cv : paquete) {
+            listaPaqueteCompleto.removeIf(tipo -> tipo.equals(cv.getTipo()));
+        }
+        return listaPaqueteCompleto.size() == 0? 10 : 0;
     }
 
     private int descuentoPorDobles(double monto, ArrayList<Reserva> paquete){
