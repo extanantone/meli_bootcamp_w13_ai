cce09236bb52226adf979b8b34076367b7b5b5c4
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-10-29 11:56:35 -0500

    Api de numeros romanos

---
+++ Spring/demoW13/src/main/java/com/bootcamp/demoW13/DemoW13Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demoW13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoW13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoW13Application.class, args);
+	}
+
+}
---
+++ Spring/demoW13/src/test/java/com/bootcamp/demoW13/DemoW13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demoW13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoW13ApplicationTests {
+	public static void main (String[] args){
+		SpringApplication.run(DemoW13Application.class, args);
+	}
+
+}
