9ecfddf1e564265957d11570df0c856124d22e12
seescobar <sebastian.escobar@mercadolibre.com.co>
2021-10-29 18:48:57 -0500

    Did the complete demo practice

---
+++ Spring/demoWave13/src/main/java/com/example/demoWave13/DemoWave13Application.java
@@ -0,0 +1,13 @@
+package com.example.demoWave13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWave13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWave13Application.class, args);
+	}
+
+}
---
+++ Spring/demoWave13/src/test/java/com/example/demoWave13/DemoWave13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoWave13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWave13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
