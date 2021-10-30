6f86da9ef9686c2ac0d2632cd41e4d7f3ab33881
Angelica Landazabal <angelica.landazabal@mercadolibre.com.co>
2021-10-29 21:44:48 -0500

    C1P2 Ejercicio del código Morse

---
+++ Spring/C1P2_CodigoMose/src/main/java/com/C1P1/CodigoMose/CodigoMoseApplication.java
@@ -0,0 +1,13 @@
+package com.C1P1.CodigoMose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoMoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodigoMoseApplication.class, args);
+	}
+
+}
---
+++ Spring/C1P2_CodigoMose/src/test/java/com/C1P1/CodigoMose/CodigoMoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C1P1.CodigoMose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoMoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
