f06fbb632ba426b56a3bc225613b8177ec9fbe85
Feli Prado <juan.cubillo@mercadolibre.com.co>
2021-11-02 14:44:27 -0500

    :sparkles: add endpoint to conver integers to roman

--- Spring/demo/src/main/java/com/example/demo/HelloWorld.java
+++ Spring/demo/src/main/java/com/example/demo/HelloWorld.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.example.demo.utils.utils.RomanNumerals;
+
 @RestController
 public class HelloWorld {
     @GetMapping("/{name}")
     public String sayHello(@PathVariable String name){
         return "Hello World " + name;
     }
+
+@GetMapping("/roman/{num}")
+    public String toRoman(@PathVariable Integer num){
+    String romanNumber = RomanNumerals(num);
+        return "The new number in roman is: "+ romanNumber;
+}
 
 }
