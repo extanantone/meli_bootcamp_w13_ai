1acd34eca6f2dd0bcdf810915d51aba8a5144946
Rodrigo <rodrigo.dimare@mercadolibre.com>
2021-11-03 12:55:13 -0300

    Agrega validaciones: ResponseEntity.BadRequest() & ResponseEntity.status().body()

--- Spring/edad/src/main/java/com/edades/edad/controller/EdadController.java
+++ Spring/edad/src/main/java/com/edades/edad/controller/EdadController.java
@@ -1,5 +1,6 @@
 package com.edades.edad.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,20 +8,36 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Date;
 
 @RestController
 public class EdadController {
 
     @GetMapping("/edades/{dia}/{mes}/{anio}")
-    public String calcularEdad(@PathVariable() String dia, @PathVariable() String mes, @PathVariable() String anio) throws ParseException {
+    public ResponseEntity<String> calcularEdad(@PathVariable() String dia, @PathVariable() String mes, @PathVariable() String anio) throws ParseException {
 
         LocalDate dateNow =  LocalDate.now();
 
-        LocalDate birthDay = LocalDate.of(Integer.parseInt(anio), Integer.parseInt(mes) , Integer.parseInt(dia));
+        int diaInt = Integer.parseInt(dia);
+        int mesInt = Integer.parseInt(mes);
+        int anioInt = Integer.parseInt(anio);
+
+        if(diaInt <= 0)
+            return ResponseEntity.badRequest().body("El dia debe ser mayor a 0");
+
+        if(mesInt <= 0 || mesInt > 12)
+            return ResponseEntity.badRequest().body("El mes debe ser mayor a 0 y menor o igual a 12");
+
+        if(anioInt < 1400 || anioInt > dateNow.getYear())
+            return ResponseEntity.badRequest().body("El anio debe ser mayor a 1400 y menor o igual a " + dateNow.getYear());
+
+
+        LocalDate birthDay = LocalDate.of(anioInt, mesInt, diaInt);
 
         Period p = Period.between(birthDay, dateNow);
 
-        return "Tenés " + String.valueOf(p.getYears()) + " años";
+        return ResponseEntity.status(200)
+                             .body("Tenés " + String.valueOf(p.getYears()) + " años");
 
     }
 }
