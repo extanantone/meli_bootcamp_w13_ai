ceeed17f86a660182d0f7d70a6f5519af4b02ee4
jdguerrerop <juan.jguerrero@mercadolibre.com.co>
2021-11-03 12:51:55 -0500

    Add ResponseEntity and personal headers

--- Spring/APIEdad/src/main/java/com/apiedad/apiedad/controller/dateController.java
+++ Spring/APIEdad/src/main/java/com/apiedad/apiedad/controller/dateController.java
@@ -1,13 +1,11 @@
 package com.apiedad.apiedad.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api/date")
@@ -15,7 +13,7 @@ public class dateController {
 
 //url/api/date/{day}/{month}/{year}
     @GetMapping(path = "/{day}/{month}/{year}")
-    public String read(@PathVariable int day,
+    public ResponseEntity<?> read(@PathVariable int day,
                        @PathVariable int month,
                        @PathVariable int year){
 
@@ -23,10 +21,32 @@ public class dateController {
 
         LocalDate past = LocalDate.of(year,month,day);
 
-       // int edad = now.getYear() - past.getYear();
+        return (past.isBefore(now)) ? ResponseEntity.ok()
+                .header("Calculadora Edad", "Entrada :" + + day + "-" + month + "-" + year)
+                .body("Edad : " + past.until(now).getYears())
+                : ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La fecha ingresada corresponde a un punto futuro en el tiempo.");
 
-        return "edad : " + past.until(now).getYears();
     }
+
+//url/api/date/
+
+    @GetMapping()
+    public ResponseEntity<?> calcular(@RequestParam int day,
+                                      @RequestParam int month,
+                                      @RequestParam int year){
+
+        HttpHeaders head = new HttpHeaders();
+
+        head.add("Calculo Edad","Entradas : " + day + "/" + month + "/" + year);
+
+        LocalDate now = LocalDate.now();
+
+        LocalDate past = LocalDate.of(year,month,day);
+
+        return (past.isBefore(now)) ? ResponseEntity.ok().headers(head).body("Edad : " + past.until(now).getYears())
+                : ResponseEntity.badRequest().body("La fecha ingresada corresponde a un punto futuro en el tiempo.");
+    }
+
 
 
 }
