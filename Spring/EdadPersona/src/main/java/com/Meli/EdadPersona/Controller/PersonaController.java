d6c2d03b05bd13b3cebe74f6ac4966020d363416
vagarro <vanesa.garro@mercadolibre.com>
2021-11-03 10:06:14 -0300

    Ejercicio en vivo Edad de una Persona

---
+++ Spring/EdadPersona/src/main/java/com/Meli/EdadPersona/Controller/PersonaController.java
@@ -0,0 +1,16 @@
+package com.Meli.EdadPersona.Controller;
+
+import com.Meli.EdadPersona.Service.PersonaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PersonaController {
+
+    @GetMapping("/Edad/{day}/{month}/{year}")
+    public long obtenerEdad(@PathVariable int day,@PathVariable int month,@PathVariable int year) {
+        return PersonaService.calcularEdad(day, month, year);
+    }
+}
---
+++ Spring/EdadPersona/src/main/java/com/Meli/EdadPersona/Service/PersonaService.java
@@ -0,0 +1,14 @@
+package com.Meli.EdadPersona.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class PersonaService {
+    public static long calcularEdad(int day, int month, int year){
+        LocalDate fHoy= LocalDate.now();
+        LocalDate cumple= LocalDate.of(year, month, day);
+        long edad= ChronoUnit.YEARS.between(cumple, fHoy);
+        return edad;
+    }
+}
