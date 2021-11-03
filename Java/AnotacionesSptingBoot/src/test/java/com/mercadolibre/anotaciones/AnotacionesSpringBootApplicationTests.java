1ecdb60960d99db863aa637927cee34b03ebb254
Malbornoz94 <magali.albornoz@mercadolibre.com>
2021-11-03 12:59:17 -0300

    Practica SpringBoot edad, uso de los verbs

---
+++ Java/AnotacionesSptingBoot/src/test/java/com/mercadolibre/anotaciones/AnotacionesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.anotaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacionesSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
