20b74c4441c184c4ef8d19ce6c32791df5037d13
Kevin <kevin.sueldo@mercadolibre,com>
2021-11-03 14:51:41 -0300

    se resuelve ejercicio spring2mañana

---
+++ Spring/EdadPersona/src/main/java/com/Ejercicio/EdadPersona/controllers/CalcularEdad.java
@@ -0,0 +1,32 @@
+package com.Ejercicio.EdadPersona.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+public class CalcularEdad {
+
+
+    @GetMapping( "edad/{day}/{month}/{year}")
+
+    public String CalcularEdad (@PathVariable int day,
+                                @PathVariable int month,
+                                @PathVariable int year)
+    {
+        LocalDate yearPersona = LocalDate.of(year,month,day);
+        LocalDate today = LocalDate.now();
+
+        Period period = Period.between(yearPersona, today);
+
+
+        System.out.println(yearPersona);
+        System.out.println(today);
+        System.out.println(period);
+        return "La edad de la persona es : " + period.getYears();
+    }
+}
