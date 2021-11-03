1ee5fd2e4abbd6d1c1f92e1d7bdb067ca527d30e
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-11-03 09:58:49 -0500

    ejercicio edad

---
+++ Spring/ejercicioEdad/src/main/java/com/example/ejercicioEdad/controller/controllerEdad.java
@@ -0,0 +1,26 @@
+package com.example.ejercicioEdad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class controllerEdad {
+    @GetMapping(path="/edad/{dia}/{mes}/{anio}")
+    public int edad(@PathVariable("dia") int dia, @PathVariable("mes") int mes, @PathVariable("anio") int anio){
+        String fecha= ""+dia+"/"+mes+"/"+anio;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fechaNacimiento = LocalDate.parse(fecha, formatter);
+        Period edad = Period.between(fechaNacimiento, LocalDate.now());
+        System.out.println(String.format("%d años, %d meses y %d días",
+                edad.getYears(),
+                edad.getMonths(),
+                edad.getDays()));
+
+        return edad.getYears();
+    }
+}
