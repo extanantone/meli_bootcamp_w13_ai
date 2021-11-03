457942a3c4c07d9729104adbc369f4d0f19382d8
rubenbuelvas-MELI <ruben.buelvas@mercadolibre.com.co>
2021-11-03 10:08:08 -0500

    Spring age calculator

---
+++ Spring/AgeCalculator/src/main/java/com/bootcamp/agecalculator/AgecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.agecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgecalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgecalculatorApplication.class, args);
+    }
+
+}
---
+++ Spring/AgeCalculator/src/test/java/com/bootcamp/agecalculator/AgecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.agecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgecalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
