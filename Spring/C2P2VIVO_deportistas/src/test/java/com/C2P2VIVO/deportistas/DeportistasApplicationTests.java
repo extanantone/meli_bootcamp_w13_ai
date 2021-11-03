a703323a02478acc623a27b327707f9bb55fb47c
Angelica Landazabal <angelica.landazabal@mercadolibre.com.co>
2021-11-03 14:15:30 -0500

    Iniciando ejercicio C2P2 Deportistas

---
+++ Spring/C2P2VIVO_deportistas/src/test/java/com/C2P2VIVO/deportistas/DeportistasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C2P2VIVO.deportistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeportistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
