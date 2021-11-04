08afdc8a422154ba9fc3785d416af7be5244a58d
Birustia1 <brian.irustia@mercadolibre.com>
2021-11-03 23:33:50 -0300

    Edad Persona

--- Spring/edadPersona/src/main/java/com/w13/edadPersona/controller/EdadController.java
+++ Spring/edadPersona/src/main/java/com/w13/edadPersona/controller/EdadController.java
@@ -1,2 +1,32 @@
-package com.w13.edadPersona.controler;public class EdadController {
+package com.w13.edadPersona.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class EdadController {
+
+    @GetMapping(path = "{dia}/{mes}/{anio}")
+    public Integer obtenerEdadDeFecha(@PathVariable("dia") int inDia,
+                                      @PathVariable("mes") int inMes,
+                                      @PathVariable("anio") int inAnio) {
+
+        LocalDate fechaNacimineto = LocalDate.of(inAnio, inMes, inDia);
+        int fechaHoy = LocalDate.now().compareTo(fechaNacimineto);
+
+        return fechaHoy;
+
+    }
+
+
+    @GetMapping("/holaMundo")
+    ResponseEntity<String> holaMundo(){
+        return ResponseEntity.ok("Hola Mundo !!");
+    }
+
+
 }
