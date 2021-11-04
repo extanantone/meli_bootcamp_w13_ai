37f6b44259399e25df05166133df43379612146b
JaySMeli <jeisson.sanchez@mercadolibre.com.co>
2021-11-04 09:31:11 -0500

    renames

--- Spring/StarWara/src/main/java/com/starwars/repository/PersonajeRepository.java
+++ Spring/StarWara/src/main/java/com/starwars/repository/PersonajeRepository.java
@@ -2,23 +2,20 @@ package com.starwars.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.starwars.model.Personaje;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class PersonajeRepositoryImpl implements IPersonajeRepository{
+public class PersonajeRepository implements IPersonajeRepository{
 
     private ObjectMapper mapper= new ObjectMapper();
 
     private List<Personaje> personajes;
 
-    public PersonajeRepositoryImpl(){
+    public PersonajeRepository(){
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("starwars_characters.json")));
             String s;
--- Spring/StarWara/src/main/java/com/starwars/service/PersonajeService.java
+++ Spring/StarWara/src/main/java/com/starwars/service/PersonajeService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class PersonajeServiceImpl implements IPersonajeService{
+public class PersonajeService implements IPersonajeService{
 
     @Autowired
     private IPersonajeRepository repository;
