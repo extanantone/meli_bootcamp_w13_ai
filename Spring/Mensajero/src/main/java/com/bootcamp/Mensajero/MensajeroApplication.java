0444f1edf0d9718d61d000d390ec325842facefe
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-11-09 16:32:26 -0500

    Parte coolaborativa mensajero

---
+++ Spring/Mensajero/src/main/java/com/bootcamp/Mensajero/MensajeroApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.Mensajero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensajeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensajeroApplication.class, args);
+	}
+
+}
---
+++ Spring/Mensajero/src/main/java/com/bootcamp/Mensajero/repository/IMensajeroRepository.java
@@ -0,0 +1,9 @@
+package com.bootcamp.Mensajero.repository;
+
+import com.bootcamp.Mensajero.model.Mensajero;
+
+import java.util.List;
+
+public interface IMensajeroRepository {
+    List<Mensajero> listarMensajeros();
+}
