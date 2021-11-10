8541b2be76416cfc50d74c191ac84e72cd0d6ad1
aleferreroml <alejandro.ferrero@mercadolibre.com>
2021-11-10 13:57:03 -0300

    agrego nuevo manejo de excepciones en ej link tracker

--- Spring/link_tracker/src/main/java/com/bootcamp/link_tracker/excepciones/GeneralHandlerException.java
+++ Spring/link_tracker/src/main/java/com/bootcamp/link_tracker/excepciones/GeneralHandlerException.java
@@ -3,6 +3,7 @@ package com.bootcamp.link_tracker.excepciones;
 import com.bootcamp.link_tracker.dto.ExceptionDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,12 @@ public class GeneralHandlerException {
     public ResponseEntity<?> passwordIncorrectException(Exception e){
         ExceptionDTO excepcion = new ExceptionDTO(e.getMessage());
         return new ResponseEntity<>(excepcion, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> missingServletRequestParameterException(Exception e){
+        ExceptionDTO excepcion = new ExceptionDTO("Debe especificar la password");
+        return new ResponseEntity<>(excepcion, HttpStatus.BAD_REQUEST);
     }
 
 }
