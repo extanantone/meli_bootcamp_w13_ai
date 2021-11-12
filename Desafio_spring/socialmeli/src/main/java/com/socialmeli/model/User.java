b44de5d6edd4aa99107368f7165c07be6f41a8bc
JaySMeli <jeisson.sanchez@mercadolibre.com.co>
2021-11-12 08:50:02 -0500

    followed user

--- Desafio_spring/socialmeli/src/main/java/com/socialmeli/model/User.java
+++ Desafio_spring/socialmeli/src/main/java/com/socialmeli/model/User.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -34,7 +35,21 @@ public class User {
     public void addFollower(User follower){
         if(!seller)
             throw new InvalidSellerException("Not allow follow user");
+        else if(followers.contains(follower))
+            throw new InvalidSellerException("Now follow this user");
         followers.add(follower);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
