7c065f21b912165369b81004f732fd548e1f1924
seescobar <sebastian.escobar@mercadolibre.com.co>
2021-11-16 12:38:26 -0500

    Changed the logic in the sorting of posts so it will actually sort them out whenever there are 2 or more, added readme briefly explaining each end point

--- Desafio_spring/SocialMeli/src/main/java/com/example/socialmeli/controller/PostController.java
+++ Desafio_spring/SocialMeli/src/main/java/com/example/socialmeli/controller/PostController.java
@@ -8,18 +8,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/products")
 public class PostController {
     IPostService postService;
 
     public PostController(IPostService postService) { this.postService = postService; }
 
-    @PostMapping("/products/post")
+    @PostMapping("/post")
     public ResponseEntity<?> postPost(@RequestBody PostDTO post) {
         postService.addPost(post.getUserId(), post.getIdPost(), post);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/products/followed/{user_id}/list")
+    @GetMapping("/followed/{user_id}/list")
     public ResponseEntity<UserPostDTO> getPosts(@PathVariable(value = "user_id")Integer userId,
                                                 @RequestParam(required = false) String order) {
         return new ResponseEntity<>(postService.getPosts(userId, order), HttpStatus.OK);
