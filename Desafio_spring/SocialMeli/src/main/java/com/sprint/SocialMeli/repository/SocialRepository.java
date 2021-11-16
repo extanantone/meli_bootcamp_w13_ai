911348d6051a5547af72006c6cfece72c401a9b6
HTonutti <hernan.tonutti@mercadolibre.com>
2021-11-16 15:18:25 -0300

    Seteo de los datos creados en el repo tal como lo piden los profesores

--- Desafio_spring/SocialMeli/src/main/java/com/sprint/SocialMeli/repository/SocialRepository.java
+++ Desafio_spring/SocialMeli/src/main/java/com/sprint/SocialMeli/repository/SocialRepository.java
@@ -22,21 +22,21 @@ public class SocialRepository implements ISocialRepository{
         // Inicialización de datos del repositorio para poder realizar algunas pruebas
         Buyer buyer1 = new Buyer(0, "Pepe");
         Buyer buyer2 = new Buyer(1, "Brenda");
-        Post post1 = new Post(3, 0, LocalDate.now(), 45, "Plancha para ropa", "Electro", "Eterna", "Blanco", "Una plaaanchaza", 23, 3400D, false, 0D);
+//        Post post1 = new Post(3, 0, LocalDate.now(), 45, "Plancha para ropa", "Electro", "Eterna", "Blanco", "Una plaaanchaza", 23, 3400D, false, 0D);
         Seller seller1 = new Seller(2, "Cacho");
         Seller seller2 = new Seller(3, "Petunia");
 
-        seller2.addPost(post1.getId_post());
-        seller1.addFollower(buyer1.getUser_id());
-        seller1.addFollower(buyer2.getUser_id());
-        buyer1.addFollowed(seller1.getUser_id());
-        buyer2.addFollowed(seller1.getUser_id());
+//        seller2.addPost(post1.getId_post());
+//        seller1.addFollower(buyer1.getUser_id());
+//        seller1.addFollower(buyer2.getUser_id());
+//        buyer1.addFollowed(seller1.getUser_id());
+//        buyer2.addFollowed(seller1.getUser_id());
 
         users.put(buyer1.getUser_id(), buyer1);
         users.put(buyer2.getUser_id(), buyer2);
         users.put(seller1.getUser_id(), seller1);
         users.put(seller2.getUser_id(), seller2);
-        posts.put(post1.getId_post(), post1);
+//        posts.put(post1.getId_post(), post1);
     }
 
     public boolean existsUser(int user_id){
