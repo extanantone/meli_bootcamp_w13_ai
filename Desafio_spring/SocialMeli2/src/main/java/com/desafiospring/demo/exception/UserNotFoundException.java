acc5be6305561006e3b2a7ecb8e551419439f14f
anaherreraMeLi <analia.herrera@mercadolibre.com>
2021-11-17 18:35:56 -0300

    Delete Desafio_spring/SocialMeli2 directory

--- Desafio_spring/SocialMeli2/src/main/java/com/desafiospring/demo/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.desafiospring.demo.exception;
-//esto es una excepcion personalizada
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(int id) {
-        super("No se encontro el usaurio " + id);
-
-    }
-}
--- Desafio_spring/SocialMeli2/src/main/java/com/desafiospring/demo/service/IFollowService.java
+++
@@ -1,6 +0,0 @@
-package com.desafiospring.demo.service;
-
-public interface IFollowService {
-    //definimos en la interfaz que necesitamos
-    public void FollowSeller(int userId, int userIdToFollow);
-}
