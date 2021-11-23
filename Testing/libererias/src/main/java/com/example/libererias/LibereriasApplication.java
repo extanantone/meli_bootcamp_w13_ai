5f76699119f989303a69d3bc100f9918ae66476d
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-11-23 15:10:05 -0500

    Test Calorias

---
+++ Testing/libererias/src/main/java/com/example/libererias/LibereriasApplication.java
@@ -0,0 +1,13 @@
+package com.example.libererias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibereriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibereriasApplication.class, args);
+	}
+
+}
---
+++ Testing/libererias/src/test/java/com/example/libererias/LibereriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libererias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibereriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
