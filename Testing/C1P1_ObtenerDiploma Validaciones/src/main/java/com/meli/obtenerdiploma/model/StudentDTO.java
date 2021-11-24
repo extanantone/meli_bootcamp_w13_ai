43cda4c44be61658f008b062752b375129cc9858
Angelica Landazabal <angelica.landazabal@mercadolibre.com.co>
2021-11-24 13:51:53 -0500

    Manejo de excepciones en Obtener diploma C1P1

--- Testing/C1P1_ObtenerDiploma Validaciones/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ Testing/C1P1_ObtenerDiploma Validaciones/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -1,7 +1,9 @@
 package com.meli.obtenerdiploma.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
@@ -11,10 +13,12 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDTO {
 
     @NotBlank(message = "El nombre del alumno no puede estar vacío.")
-    @Pattern( regexp = "^[A-Z][A-Za-z]*$", message = "El nombre debe de empezar con mayúsculas.")
+    @Pattern( regexp = "([A-Z])[A-Za-z]*$", message = "El nombre debe de empezar con mayúsculas.")
     @Size(max = 50, message = "La longitud debe de ser 50 caracteres.")
     String studentName;
 
