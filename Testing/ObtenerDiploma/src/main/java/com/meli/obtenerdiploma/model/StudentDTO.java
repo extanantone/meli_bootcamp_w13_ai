8574b3c038eb2a7caa29a7011134b06520976945
juandavidortizv <juan.ortiz@mercadolibre.com.co>
2021-11-24 18:23:26 -0500

    Ejercicio 1 Validación de los campos de entrada

--- Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -4,12 +4,33 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
+
+    /*
+    Longitud máxima de 50 caracteres.
+    Que empiece con mayúscula.
+    Que el campo no esté vacío.
+     */
+
+    @Size(min=1, max=50, message = "La longitud del nombre no puede superar los {max} caracteres.")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|A-Z|a-z|ñ|á|é|í|ó|ú|Á|É|Í|Ó|Ú]*$", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
     String studentName;
     String message;
     Double averageScore;
-    List<SubjectDTO> subjects;
+
+    /*
+    Que la lista no esté vacía.
+     */
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    List<@Valid SubjectDTO> subjects;
 }
+
--- Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ Testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -3,8 +3,28 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter @Setter
 public class SubjectDTO {
+
+    /*
+    Que el campo no esté vacío.
+    Longitud máxima de 30 caracteres.
+    Que empiece con mayúscula.
+     */
+    @NotBlank(message = "El nombre de la materia no puede estar vacío.")
+    @Size(min=1, max=30, message = "La longitud del nombre no puede superar los {max} caracteres.")
+    @Pattern(regexp = "([A-Z]|[0-9])[\\s|A-Z|a-z|ñ|á|é|í|ó|ú|Á|É|Í|Ó|Ú]*$",message = "El nombre de la materia debe comenzar con mayúscula.")
     String name;
+
+    /*
+    Que el campo no esté vacío.
+    Mínimo 0.0
+    Máximo 10-0
+     */
+    @NotNull(message = "La nota no puede estar vacía.")
+    @DecimalMin(value = "0.0", message = "La mínima nota es {value}")
+    @DecimalMax(value = "10.0", message = "La máxima nota es {value}")
     Double score;
 }
