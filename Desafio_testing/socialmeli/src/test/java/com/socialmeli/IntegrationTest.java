35fc28401036ff33b1091e22621037f3ece2da98
JaySMeli <jeisson.sanchez@mercadolibre.com.co>
2021-11-25 15:44:41 -0500

    integration and validations

--- Desafio_testing/socialmeli/src/test/java/com/socialmeli/IntegrationTest.java
+++ Desafio_testing/socialmeli/src/test/java/com/socialmeli/IntegrationTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import java.util.ArrayList;
@@ -117,22 +120,117 @@ public class IntegrationTest {
 
     @Test
     public void shouldntBeUnfollowUnexistUser() throws Exception{
-        mock.perform(post("/users/4/unfollow/100"))
+        mock.perform(post("/users/4/unfollow/100")).andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isNotFound());
     }
 
     @Test
     public void shouldBeUnfollowUser() throws Exception{
-        mock.perform(post("/users/2/follow/4")).andExpect(
+        mock.perform(post("/users/2/follow/4"))
+                .andDo(MockMvcResultHandlers.print()).andExpect(
                 status().is2xxSuccessful());
         mock.perform(post("/users/2/unfollow/4"))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().is2xxSuccessful());
     }
 
     @Test
     public void shouldntBeUnfollowNegativeUsers() throws Exception{
         mock.perform(post("/users/2/unfollow/-2"))
+                .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldntBeCountFollowerList() throws Exception{
+        mock.perform(get("/users/70/followers/count"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldBeCountFollowers() throws Exception{
+        mock.perform(post("/users/2/follow/4"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is2xxSuccessful());
+        mock.perform(get("/users/4/followers/count"))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.followers_count").value(1))
+                 .andExpect(jsonPath("$.user_id").value(4));
+    }
+
+    @Test
+    public  void shouldntBeCountFollowersOfNegativeUser() throws Exception{
+        mock.perform(get("/users/-70/followers/count"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public  void shouldntBeCountFollowersNotSeller() throws Exception{
+        mock.perform(get("/users/1/followers/count"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void  shouldntBeListFollowersUnexistUser() throws Exception{
+        mock.perform(get("/users/70/followers/list"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void  shouldntBeListFollowersWithNegativeUserId() throws Exception{
+        mock.perform(get("/users/-70/followers/list"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldntBeListFollowersIfUserIsntSeller() throws Exception{
+        mock.perform(get("/users/1/followers/list"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldBeListUsers() throws Exception{
+        mock.perform(post("/users/2/follow/4"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is2xxSuccessful());
+        mock.perform(get("/users/4/followers/list"))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.followers[0].user_id").value(2))
+                .andExpect(jsonPath("$.user_id").value(4));
+    }
+
+    @Test
+    public void shouldBeListFollowedList() throws Exception{
+        mock.perform(post("/users/2/follow/4"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is2xxSuccessful());
+        mock.perform(get("/users/2/followed/list"))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.followed[0].user_id").value(4))
+                .andExpect(jsonPath("$.user_id").value(2));
+    }
+
+    @Test
+    public void shouldntBeListFollowedListIfUserUnexist() throws Exception{
+        mock.perform(get("/users/90/followed/list"))
+                .andExpect(status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void  shouldntBeListNegativeUser() throws Exception{
+        mock.perform(get("/users/-90/followed/list"))
+                .andExpect(status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
     }
 
 }
