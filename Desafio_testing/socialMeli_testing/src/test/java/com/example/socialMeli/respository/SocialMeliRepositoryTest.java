e8d479a2393339a90acd8be0157061d18ebdfd89
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-11-25 14:41:24 -0500

    test 2 y repo

--- Desafio_testing/socialMeli_testing/src/test/java/com/example/socialMeli/respository/SocialMeliRepositoryTest.java
+++ Desafio_testing/socialMeli_testing/src/test/java/com/example/socialMeli/respository/SocialMeliRepositoryTest.java
@@ -1,4 +1,40 @@
 package com.example.socialMeli.respository;
 
+import com.example.socialMeli.model.Comprador;
+import com.example.socialMeli.model.Publicacion;
+import com.example.socialMeli.model.Vendedor;
+import com.example.socialMeli.repository.ISocialMeliRepository;
+import com.example.socialMeli.repository.SocialMeliRepository;
+import com.example.socialMeli.service.SocialMeliService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SocialMeliRepositoryTest {
+    SocialMeliRepository repo;
+    @BeforeEach
+    void cargar(){
+        this.repo = new SocialMeliRepository();
+        repo.agregarCompradores();
+        repo.agregarVendedores();
+    }
+
+    @Test
+    void UsuarioASeguirYADejarDeSeguirValido(){
+        Vendedor expected = new Vendedor("vendedor 1", 3);
+        Vendedor actual=repo.buscarVendedor(expected.getUser_id());
+        assertAll("vendedor", () -> assertEquals(expected.getName(), actual.getName()),
+                () -> assertEquals(expected.getUser_id(), actual.getUser_id()));
+    }
+    @Test
+    void UsuarioASeguirYADejarDeSeguirNoValido(){
+        Vendedor actual=repo.buscarVendedor(60);
+        assertEquals(null,actual);
+
+    }
 }
--- Desafio_testing/socialMeli_testing/src/test/java/com/example/socialMeli/service/SocialMeliServiceTest.java
+++ Desafio_testing/socialMeli_testing/src/test/java/com/example/socialMeli/service/SocialMeliServiceTest.java
@@ -40,17 +40,15 @@ public class SocialMeliServiceTest {
 
         // assert
         verify(repo, atLeastOnce()).buscarVendedor(vende.getUser_id());
-        assertEquals(expected.getMensaje(), actual.getMensaje());
-
+        assertAll("rta", () -> assertEquals(expected.getStatusCode(), actual.getStatusCode()),
+                () -> assertEquals(expected.getMensaje(), actual.getMensaje()));
     }
 
     @Test
     void T_0001_NoCumple() throws UsuarioNoEncontradoError {
         // arrange
-        Boolean flag=true;
         Vendedor vende = new Vendedor("vendedor 1", 3);
         Comprador compra = new Comprador("comprador 1", 1);
-        RespuestaSimpleDTO expected= new RespuestaSimpleDTO("El comprador "+compra.getUser_id()+" ha seguido con exito al vendedor "+vende.getUser_id(), 200);
         when(repo.buscarVendedor(vende.getUser_id())).thenReturn(null);
         when(repo.buscarComprador(compra.getUser_id())).thenReturn(compra);
 
@@ -66,17 +64,30 @@ public class SocialMeliServiceTest {
         Boolean flag=true;
         Vendedor vende = new Vendedor("vendedor 1", 3);
         Comprador compra = new Comprador("comprador 1", 1);
-        RespuestaSimpleDTO expected= new RespuestaSimpleDTO("El comprador "+compra.getUser_id()+" ha seguido con exito al vendedor "+vende.getUser_id(), 200);
+        RespuestaSimpleDTO expected= new RespuestaSimpleDTO("El comprador "+compra.getUser_id()+" ha dejado de seguir con exito al vendedor "+vende.getUser_id(), 200);
         when(repo.buscarVendedor(vende.getUser_id())).thenReturn(vende);
         when(repo.buscarComprador(compra.getUser_id())).thenReturn(compra);
-        when(repo.buscarSeguidor(vende.getSeguidores(), compra.getUser_id())).thenReturn(null);
-        when(repo.seguir(compra,vende)).thenReturn(flag=true);
+        when(repo.buscarSeguidor(vende.getSeguidores(), compra.getUser_id())).thenReturn(compra);
+        when(repo.dejarDeSeguir(compra,vende)).thenReturn(flag=true);
 
         // act
-        RespuestaSimpleDTO actual = service.seguir(compra.getUser_id(), vende.getUser_id());
+        RespuestaSimpleDTO actual = service.dejarDeSeguir(compra.getUser_id(), vende.getUser_id());
 
         // assert
         verify(repo, atLeastOnce()).buscarVendedor(vende.getUser_id());
-        assertEquals(expected.getMensaje(), actual.getMensaje());
+        assertAll("rta", () -> assertEquals(expected.getStatusCode(), actual.getStatusCode()),
+                () -> assertEquals(expected.getMensaje(), actual.getMensaje()));
+    }
+    @Test
+    void T_0002_NoCumple() throws UsuarioNoEncontradoError {
+        // arrange
+        Vendedor vende = new Vendedor("vendedor 1", 3);
+        Comprador compra = new Comprador("comprador 1", 1);
+        when(repo.buscarVendedor(vende.getUser_id())).thenReturn(null);
+        when(repo.buscarComprador(compra.getUser_id())).thenReturn(compra);
+
+        // assert
+        assertThrows(UsuarioNoEncontradoError.class,()->service.dejarDeSeguir(compra.getUser_id(), vende.getUser_id()));
+        verify(repo, atLeastOnce()).buscarVendedor(vende.getUser_id());
     }
 }
