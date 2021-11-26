c483ae0bb9a05633f80399856e80daf0c26432ae
Rodrigo <rodrigo.dimare@mercadolibre.com>
2021-11-26 13:10:53 -0300

    Agrega T-0006

--- Desafio_testing/src/test/java/com/bootcamp/socialmeli/repository/RepositoryPostTest.java
+++ Desafio_testing/src/test/java/com/bootcamp/socialmeli/repository/RepositoryPostTest.java
@@ -50,4 +50,61 @@ public class RepositoryPostTest {
         });
 
     }
+
+    @Test
+    void verifyPostOrderASC(){
+
+        List<DTOPublishPost> postListOrderASC = new ArrayList<>();
+        List<DTOPublishPost> UserListNotSorted = new ArrayList<>();
+
+
+        LocalDate date1 = LocalDate.of(2021,12,5);
+        LocalDate date2 = LocalDate.of(2021,12,6);
+        LocalDate date3 = LocalDate.of(2021,12,7);
+
+        Product product = new Product(1,"Silla","Asiento","Marca","gris","prueba");
+
+        postListOrderASC.add(new DTOPublishPost(1, date1, product, 1, 100.20));
+        postListOrderASC.add(new DTOPublishPost(3, date2, product, 1, 100.20));
+        postListOrderASC.add(new DTOPublishPost(3, date3, product, 1, 100.20));
+
+        UserListNotSorted.add(new DTOPublishPost(3, date2, product, 1, 100.20));
+        UserListNotSorted.add(new DTOPublishPost(3, date3, product, 1, 100.20));
+        UserListNotSorted.add(new DTOPublishPost(1, date1, product, 1, 100.20));
+
+        UserListNotSorted = iPostRepository.orderPosts(UserListNotSorted,"date_asc");
+
+        Assertions.assertEquals(postListOrderASC,UserListNotSorted);
+
+
+    }
+
+
+    @Test
+    void verifyPostOrderDESC(){
+
+        List<DTOPublishPost> postListOrderDESC = new ArrayList<>();
+        List<DTOPublishPost> UserListNotSorted = new ArrayList<>();
+
+
+        LocalDate date1 = LocalDate.of(2021,12,5);
+        LocalDate date2 = LocalDate.of(2021,12,6);
+        LocalDate date3 = LocalDate.of(2021,12,7);
+
+        Product product = new Product(1,"Silla","Asiento","Marca","gris","prueba");
+
+        postListOrderDESC.add(new DTOPublishPost(3, date3, product, 1, 100.20));
+        postListOrderDESC.add(new DTOPublishPost(2, date2, product, 1, 100.20));
+        postListOrderDESC.add(new DTOPublishPost(1, date1, product, 1, 100.20));
+
+        UserListNotSorted.add(new DTOPublishPost(1, date1, product, 1, 100.20));
+        UserListNotSorted.add(new DTOPublishPost(3, date3, product, 1, 100.20));
+        UserListNotSorted.add(new DTOPublishPost(2, date2, product, 1, 100.20));
+
+        UserListNotSorted = iPostRepository.orderPosts(UserListNotSorted,"date_desc");
+
+        Assertions.assertEquals(postListOrderDESC,UserListNotSorted);
+
+
+    }
 }
