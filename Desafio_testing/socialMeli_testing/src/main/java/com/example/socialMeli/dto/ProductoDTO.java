0aa705221e5ba9e78bfb4979d6ab51df939dc6af
sofiamoreno199 <angela.moreno@mercadolibre.com.co>
2021-11-26 11:57:14 -0500

    validaciones

--- Desafio_testing/socialMeli_testing/src/main/java/com/example/socialMeli/dto/ProductoDTO.java
+++ Desafio_testing/socialMeli_testing/src/main/java/com/example/socialMeli/dto/ProductoDTO.java
@@ -17,25 +17,20 @@ import javax.validation.constraints.Size;
 
 public class ProductoDTO {
     @NotNull(message = "Debe introducir un id de producto")
-    @Pattern(regexp = "^[0-9_ ]*$", message = "El id del producto debe ser un dato numérico")
-    private int product_id;
+    private Integer product_id;
     @NotNull(message = "Debe introducir el nombre del producto")
     @Size(max=40, message = "El nombre del producto no puede tener una longitur mayor a cuarenta")
-    @Pattern(regexp = "^[a-zA-Z0-9_ ]*$", message = "El nombre del producto no puede contener caracteres especiales")
-    private String product_name;
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El nombre del produc no puede poseer caracteres especiales")    private String product_name;
     @NotNull(message = "Debe introducir el tipo de producto")
     @Size(max=15, message = "El tipo no puede tener una longitud mayor a quince")
-    @Pattern(regexp = "^[a-zA-Z0-9_ ]*$", message = "El tipo no puede contener caracteres especiales")
-    private String type;
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El tipo no puede poseer caracteres especiales")    private String type;
     @NotNull(message = "Debe introducir la marca del producto")
     @Size(max=25, message = "La marca no puede tener una longitud mayor a veinticinco")
-    @Pattern(regexp = "^[a-zA-Z0-9_ ]*$", message = "La marca no puede contener caracteres especiales")
-    private String brand;
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "La marca no puede poseer caracteres especiales")    private String brand;
     @NotNull(message = "Debe introducir el color del producto")
     @Size(max=15, message = "El color no puede tener una longitud mayor a quince")
-    @Pattern(regexp = "^[a-zA-Z0-9_ ]*$", message = "El color no puede contener caracteres especiales")
-    private String color;
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El color no puede poseer caracteres especiales")    private String color;
     @NotNull(message = "Debe añadir notas")
-    @Size(max=80, message = "Las notas no pueden tener una longitud mayor a quince")
+    @Size(max=80, message = "Las notas no pueden tener una longitud mayor a ochenta")
     private String notes;
 }
