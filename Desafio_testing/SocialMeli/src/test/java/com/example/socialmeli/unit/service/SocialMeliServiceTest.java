08c7eb5e5b345d19acfecd4e79f3621bb5a26b66
Feli Prado <juan.cubillo@mercadolibre.com.co>
2021-11-26 10:30:08 -0500

    :sparkles: check if user exist 00001

--- Desafio_testing/SocialMeli/src/test/java/com/example/socialmeli/unit/service/SocialMeliServiceTest.java
+++ Desafio_testing/SocialMeli/src/test/java/com/example/socialmeli/unit/service/SocialMeliServiceTest.java
@@ -2,7 +2,9 @@ package com.example.socialmeli.unit.service;
 
 import com.example.socialmeli.dto.UserDTO;
 import com.example.socialmeli.dto.response.FollowersResponseDTO;
+import com.example.socialmeli.exceptions.UserAlreadyInUseException;
 import com.example.socialmeli.exceptions.UserNotFoundException;
+import com.example.socialmeli.exceptions.UserSelfUseException;
 import com.example.socialmeli.model.User;
 import com.example.socialmeli.repositories.PostRepository;
 import com.example.socialmeli.repositories.UsuarioRepository;
@@ -15,13 +17,15 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
 
 @ExtendWith(MockitoExtension.class)
 public class SocialMeliServiceTest {
@@ -68,6 +72,33 @@ public class SocialMeliServiceTest {
 
         //Assert
         Mockito.verify(userRepository, times(2)).findById(Mockito.anyInt());
+        //Mockito.verify(userRepository, atLeastOnce()).findById(Mockito.anyInt());
         Assertions.assertEquals(expect, current);
+    }
+
+    @Test
+    public void checkIfTheUserToFollowExist(){
+        //arrange
+        String order = null;
+        Integer userId = 1;
+        Integer followerId = 2;
+
+
+        // Mock
+
+        User userMock = new User(userId ,"user", new ArrayList<>());
+        User followerMock = new User(followerId, "follower", new ArrayList<>());
+        // When
+        when(userRepository.findById(userId)).thenReturn(Optional.of(userMock));
+        when(userRepository.findById(followerId)).thenReturn(Optional.of(followerMock));
+
+        //Act
+        try {
+            socialMeliService.follow(userId,followerId);
+            Assertions.assertTrue(true);
+        } catch (UserNotFoundException | UserSelfUseException | UserAlreadyInUseException e) {
+            Assertions.fail();
+        }
+
     }
 }
