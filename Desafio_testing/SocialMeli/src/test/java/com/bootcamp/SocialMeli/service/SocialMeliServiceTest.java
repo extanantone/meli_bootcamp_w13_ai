c8df151c425d0d432a1edb0b918a40e9e99e3582
juandavidortizv <juan.ortiz@mercadolibre.com.co>
2021-11-27 22:32:46 -0500

    T-0001 Usuario a seguir existe completado

--- Desafio_testing/SocialMeli/src/test/java/com/bootcamp/SocialMeli/service/SocialMeliServiceTest.java
+++ Desafio_testing/SocialMeli/src/test/java/com/bootcamp/SocialMeli/service/SocialMeliServiceTest.java
@@ -3,8 +3,12 @@ package com.bootcamp.SocialMeli.service;
 import com.bootcamp.SocialMeli.entity.Buyer;
 import com.bootcamp.SocialMeli.entity.Post;
 import com.bootcamp.SocialMeli.entity.Seller;
+import com.bootcamp.SocialMeli.exception.ExceptionSellerNotExist;
 import com.bootcamp.SocialMeli.repository.UserRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -38,6 +42,54 @@ public class SocialMeliServiceTest {
         this.posts = new ArrayList<>();
 
     }
+
+    @Nested
+    @DisplayName("T-0001")
+    class T0001 {
+        @DisplayName("T-0001 Usuario a seguir existe")
+        @Test
+        public void userToFollowExistTest() {
+            //Arrange
+            int userId = 1;
+            int userToFollowId = 3;
+            user1.getFollowed().add(user3);
+            user3.getFollowers().add(user1);
+
+            when(mockUserRepository.findBuyerById(userId)).thenReturn(user1);
+            when(mockUserRepository.findSellerById(userToFollowId)).thenReturn(user3);
+            when(mockUserRepository.follow(user1,user3)).thenReturn(true);
+
+            //Act
+            assertDoesNotThrow(() -> userService.follow(userId, userToFollowId));
+            //Assert
+            assertEquals(1, user1.getFollowed().size());
+            assertEquals(1, user3.getFollowers().size());
+            assertTrue(user1.getFollowed().contains(user3));
+            assertTrue(user3.getFollowers().contains(user1));
+            verify(mockUserRepository, atLeastOnce()).findBuyerById(userId);
+            verify(mockUserRepository, atLeastOnce()).findSellerById(userToFollowId);
+            verify(mockUserRepository, (times(1))).follow(user1,user3);
+        }
+
+        @DisplayName("T-0001 Usuario a seguir No existe")
+        @Test
+        public void userToFollowNotExistTest() {
+            //Arrange
+            int userId = 1;
+            int userToFollowId = 5;
+
+            //Act
+            when(mockUserRepository.findBuyerById(userId)).thenReturn(user1);
+            when(mockUserRepository.findSellerById(userToFollowId)).thenThrow(ExceptionSellerNotExist.class);
+
+            //Assert
+            assertThrows(ExceptionSellerNotExist.class,
+                    ()-> userService.follow(userId,userToFollowId), "Usuario no existe");
+            verify(mockUserRepository, atLeastOnce()).findBuyerById(userId);
+            verify(mockUserRepository, atLeastOnce()).findSellerById(userToFollowId);
+        }
+    }
+
 
 }
 
