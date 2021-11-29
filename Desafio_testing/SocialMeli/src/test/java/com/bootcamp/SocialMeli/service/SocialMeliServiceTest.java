eb88cccd310058095561d34720cf93a5d6febca2
juandavidortizv <juan.ortiz@mercadolibre.com.co>
2021-11-28 20:19:24 -0500

    T-0004 ordenamiento lista seguidores y seguidos completado

--- Desafio_testing/SocialMeli/src/test/java/com/bootcamp/SocialMeli/service/SocialMeliServiceTest.java
+++ Desafio_testing/SocialMeli/src/test/java/com/bootcamp/SocialMeli/service/SocialMeliServiceTest.java
@@ -1,5 +1,8 @@
 package com.bootcamp.SocialMeli.service;
 
+import com.bootcamp.SocialMeli.dto.FollowedListDTO;
+import com.bootcamp.SocialMeli.dto.FollowerListDTO;
+import com.bootcamp.SocialMeli.dto.UserDTO;
 import com.bootcamp.SocialMeli.entity.Buyer;
 import com.bootcamp.SocialMeli.entity.Post;
 import com.bootcamp.SocialMeli.entity.Seller;
@@ -197,6 +200,187 @@ public class SocialMeliServiceTest {
 
         }
 
+    }
+
+    @Nested
+    @DisplayName("T-0004")
+    class T0004 {
+        @DisplayName("T-0004 ordenamiento ListFollowers por defecto ascendente por nombre")
+        @Test
+        void orderDefaultListFollowersTest() {
+            int userId = 4;
+
+            user4.getFollowers().add(user1); //Leon Comprador -> Sigue a Pedro Vendedor
+            user4.getFollowers().add(user2); //Juan Comprador -> Sigue a Pedro Vendedor
+
+            ArrayList<UserDTO> ListFollowers = new ArrayList<>();
+            ListFollowers.add(new UserDTO(user2.getUserId(), user2.getUserName()));
+            ListFollowers.add(new UserDTO(user1.getUserId(), user1.getUserName()));
+            FollowerListDTO expected = new FollowerListDTO();
+            expected.setFollowers(ListFollowers);
+            expected.setUser_id(userId);
+            expected.setUser_name(user4.getUserName());
+
+            when(mockUserRepository.findSellerById(userId)).thenReturn(user4);
+
+            //Act
+            FollowerListDTO actual = userService.followers(userId, null);
+
+            //Assert
+            assertEquals(expected,actual);
+            verify(mockUserRepository, atLeastOnce()).findSellerById(userId);
+            verify(mockUserRepository, never()).findBuyerById(userId);
+        }
+        @DisplayName("T-0004 ordenamiento ListFollowers descendente por nombre")
+        @Test
+        void orderDescListFollowersTest() {
+            int userId = 4;
+            String order = "name_desc";
+
+            user4.getFollowers().add(user1); //Leon Comprador -> Sigue a Pedro Vendedor
+            user4.getFollowers().add(user2); //Juan Comprador -> Sigue a Pedro Vendedor
+
+            ArrayList<UserDTO> ListFollowers = new ArrayList<>();
+
+            ListFollowers.add(new UserDTO(user1.getUserId(), user1.getUserName()));
+            ListFollowers.add(new UserDTO(user2.getUserId(), user2.getUserName()));
+
+            FollowerListDTO expected = new FollowerListDTO();
+            expected.setFollowers(ListFollowers);
+            expected.setUser_id(userId);
+            expected.setUser_name(user4.getUserName());
+
+            when(mockUserRepository.findSellerById(userId)).thenReturn(user4);
+
+            //Act
+            FollowerListDTO actual = userService.followers(userId, order);
+
+            //Assert
+            assertEquals(expected,actual);
+            verify(mockUserRepository, atLeastOnce()).findSellerById(userId);
+            verify(mockUserRepository, never()).findBuyerById(userId);
+        }
+
+        @DisplayName("T-0004 ordenamiento ListFollowers ascendente por nombre")
+        @Test
+        void orderAscListFollowersTest() {
+            int userId = 4;
+            String order = "name_asc";
+
+            user4.getFollowers().add(user1); //Leon Comprador -> Sigue a Pedro Vendedor
+            user4.getFollowers().add(user2); //Juan Comprador -> Sigue a Pedro Vendedor
+
+            ArrayList<UserDTO> ListFollowers = new ArrayList<>();
+
+            ListFollowers.add(new UserDTO(user2.getUserId(), user2.getUserName()));
+            ListFollowers.add(new UserDTO(user1.getUserId(), user1.getUserName()));
+
+            FollowerListDTO expected = new FollowerListDTO();
+            expected.setFollowers(ListFollowers);
+            expected.setUser_id(userId);
+            expected.setUser_name(user4.getUserName());
+
+            when(mockUserRepository.findSellerById(userId)).thenReturn(user4);
+
+            //Act
+            FollowerListDTO actual = userService.followers(userId, order);
+
+            //Assert
+            assertEquals(expected,actual);
+            verify(mockUserRepository, atLeastOnce()).findSellerById(userId);
+            verify(mockUserRepository, never()).findBuyerById(userId);
+        }
+
+        @DisplayName("T-0004 ordenamiento ListFollowed por defecto ascendente por nombre ")
+        @Test
+        void orderDefaultListFollowedTest() {
+            int userId = 1;
+
+            user1.getFollowed().add(user3);
+            user1.getFollowed().add(user4);
+
+            ArrayList<UserDTO> ListFollowed = new ArrayList<>();
+
+            ListFollowed.add(new UserDTO(user3.getUserId(), user3.getUserName()));
+            ListFollowed.add(new UserDTO(user4.getUserId(), user4.getUserName()));
+
+            FollowedListDTO expected = new FollowedListDTO();
+            expected.setFollowed(ListFollowed);
+            expected.setUser_id(userId);
+            expected.setUser_name(user1.getUserName());
+
+            when(mockUserRepository.findBuyerById(userId)).thenReturn(user1);
+
+            //Act
+            FollowedListDTO actual = userService.followed(userId, null);
+
+            //Assert
+            assertEquals(expected,actual);
+            verify(mockUserRepository, atLeastOnce()).findBuyerById(userId);
+            verify(mockUserRepository, never()).findSellerById(userId);
+
+        }
+
+        @DisplayName("T-0004 ordenamiento ListFollowed  ascendente por nombre ")
+        @Test
+        void orderAscListFollowedTest() {
+            int userId = 1;
+            String order = "name_asc";
+
+            user1.getFollowed().add(user3);
+            user1.getFollowed().add(user4);
+
+            ArrayList<UserDTO> ListFollowed = new ArrayList<>();
+
+            ListFollowed.add(new UserDTO(user3.getUserId(), user3.getUserName()));
+            ListFollowed.add(new UserDTO(user4.getUserId(), user4.getUserName()));
+
+            FollowedListDTO expected = new FollowedListDTO();
+            expected.setFollowed(ListFollowed);
+            expected.setUser_id(userId);
+            expected.setUser_name(user1.getUserName());
+
+            when(mockUserRepository.findBuyerById(userId)).thenReturn(user1);
+
+            //Act
+            FollowedListDTO actual = userService.followed(userId, order);
+
+            //Assert
+            assertEquals(expected,actual);
+            verify(mockUserRepository, atLeastOnce()).findBuyerById(userId);
+            verify(mockUserRepository, never()).findSellerById(userId);
+        }
+
+        @DisplayName("T-0004 ordenamiento ListFollowed  descendente por nombre ")
+        @Test
+        void orderDescListFollowedTest() {
+            int userId = 1;
+            String order = "name_desc";
+
+            user1.getFollowed().add(user3);
+            user1.getFollowed().add(user4);
+
+            ArrayList<UserDTO> ListFollowed = new ArrayList<>();
+
+            ListFollowed.add(new UserDTO(user4.getUserId(), user4.getUserName()));
+            ListFollowed.add(new UserDTO(user3.getUserId(), user3.getUserName()));
+
+            FollowedListDTO expected = new FollowedListDTO();
+            expected.setFollowed(ListFollowed);
+            expected.setUser_id(userId);
+            expected.setUser_name(user1.getUserName());
+
+            when(mockUserRepository.findBuyerById(userId)).thenReturn(user1);
+
+            //Act
+            FollowedListDTO actual = userService.followed(userId, order);
+
+            //Assert
+            assertEquals(expected,actual);
+            //assertArrayEquals(expected.getFollowed().toArray(),actual.getFollowed().toArray());
+            verify(mockUserRepository, atLeastOnce()).findBuyerById(userId);
+            verify(mockUserRepository, never()).findSellerById(userId);
+        }
     }
 
 
