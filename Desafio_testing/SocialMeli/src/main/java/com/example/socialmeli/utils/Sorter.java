65a271a8130cd1c29ef135593f80420bb2bfce09
Jose Andres Iglesias Leal <jose.iglesias@mercadolibre.com.co>
2021-11-29 10:10:58 -0500

    Delete Desafio_testing/SocialMeli directory

--- Desafio_testing/SocialMeli/src/main/java/com/example/socialmeli/utils/Sorter.java
+++
@@ -1,6 +0,0 @@
-package com.example.socialmeli.utils;
-
-@FunctionalInterface
-public interface Sorter <T>{
-    int sort(T first , T second);
-}
--- Desafio_testing/SocialMeli/src/test/java/com/example/socialmeli/unit/Repository.java
+++
@@ -1,4 +0,0 @@
-package com.example.socialmeli.unit;
-
-public class Repository {
-}
