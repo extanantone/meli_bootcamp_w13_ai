2475b5a919d2ea8709e7a95a21a7ca724c899b5b
Cristian Díaz <cristian.diaz@mercadolibre.com.co>
2021-11-29 09:15:49 -0500

    Punto 2 testing

---
+++ Desafio_testing/SocialMeli/src/test/java/com/MeLi/SocialMeli/service/CompradorServiceTest.java
@@ -0,0 +1,105 @@
+package com.MeLi.SocialMeli.service;
+
+import com.MeLi.SocialMeli.DTO.SeguimientoDTO;
+import com.MeLi.SocialMeli.controller.CompradorController;
+import com.MeLi.SocialMeli.exception.NotFoundCompradorException;
+import com.MeLi.SocialMeli.exception.NotFoundVendedorException;
+import com.MeLi.SocialMeli.model.Comprador;
+import com.MeLi.SocialMeli.model.Vendedor;
+import com.MeLi.SocialMeli.repository.CompradorRepositoryImplement;
+import com.MeLi.SocialMeli.repository.VendedorRepositoryImplement;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class CompradorServiceTest {
+
+    //@Mock
+    //private CompradorServiceImplement mockCompradorService;
+
+    @Mock
+    private CompradorRepositoryImplement mockCompradorRepository;
+
+    @Mock
+    private VendedorRepositoryImplement mockVendedorRepository;
+
+    @InjectMocks
+    private CompradorService compradorService;
+
+    @Test
+    void if_follow_is_success() throws NotFoundVendedorException, NotFoundCompradorException {
+
+        //arrange
+        Comprador comprador = new Comprador(1,"Pedro");
+        Mockito.when(mockCompradorRepository.find(1)).thenReturn(Optional.of(comprador));
+
+        Vendedor vendedor = new Vendedor(3,"Juan");
+        Mockito.when(mockVendedorRepository.find(3)).thenReturn(Optional.of(vendedor));
+
+        //act
+        SeguimientoDTO resultado = compradorService.seguir(1,3);
+
+        //assert
+        assertEquals("Seguimiento exitoso", resultado.getMsj());
+    }
+
+    @Test
+    void if_follower_not_exist_follow() throws NotFoundVendedorException, NotFoundCompradorException {
+
+        //act
+        Mockito.when(mockCompradorRepository.find(5)).thenReturn(Optional.empty());
+        //assert
+        Assertions.assertThrows(NotFoundCompradorException.class, ()->compradorService.seguir(5,3));
+    }
+
+    @Test
+    void if_followed_not_exist_follow() throws NotFoundVendedorException, NotFoundCompradorException {
+
+        //arrange
+        Comprador comprador = new Comprador(1,"Pedro");
+
+        //Act
+        Mockito.when(mockCompradorRepository.find(1)).thenReturn(Optional.of(comprador));
+        Mockito.when(mockVendedorRepository.find(1)).thenReturn(Optional.empty());
+
+        //assert
+        Assertions.assertThrows(NotFoundVendedorException.class, ()->compradorService.seguir(1,1));
+    }
+
+
+    @Test
+    void if_unfollow_is_success() throws NotFoundVendedorException, NotFoundCompradorException {
+
+        //arrange
+
+        Comprador comprador = new Comprador(1,"Pedro");
+        Mockito.when(mockCompradorRepository.find(1)).thenReturn(Optional.of(comprador));
+
+        Vendedor vendedor = new Vendedor(3,"Juan");
+        Mockito.when(mockVendedorRepository.find(3)).thenReturn(Optional.of(vendedor));
+        //act
+
+        SeguimientoDTO resultado = compradorService.dejarSeguir(1,3);
+        //assert
+        assertEquals(comprador.getNombre() + " has dejado de seguir a " + vendedor.getNombre(), resultado.getMsj());
+    }
+
+    @Test
+    void if_unfollowed_not_exist() throws NotFoundVendedorException, NotFoundCompradorException{
+
+        Comprador comprador = new Comprador(1,"Pedro");
+        Mockito.when(mockCompradorRepository.find(1)).thenReturn(Optional.of(comprador));
+    }
+}
--- Desafio_testing/SocialMeli/src/test/java/com/MeLi/SocialMeli/service/PublicacionControllerTest.java
+++ Desafio_testing/SocialMeli/src/test/java/com/MeLi/SocialMeli/service/PublicacionControllerTest.java
@@ -1,4 +1,4 @@
-package com.MeLi.SocialMeli.controller;
+package com.MeLi.SocialMeli.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
