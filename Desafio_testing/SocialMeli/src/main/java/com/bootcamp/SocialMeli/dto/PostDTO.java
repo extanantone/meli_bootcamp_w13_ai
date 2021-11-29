450ff0ffe0e285c5ce3194b71238f1bfd6037545
juandavidortizv <juan.ortiz@mercadolibre.com.co>
2021-11-29 08:56:36 -0500

    Validaciones agregadas a PostDTO.java

--- Desafio_testing/SocialMeli/src/main/java/com/bootcamp/SocialMeli/dto/PostDTO.java
+++ Desafio_testing/SocialMeli/src/main/java/com/bootcamp/SocialMeli/dto/PostDTO.java
@@ -1,9 +1,11 @@
 package com.bootcamp.SocialMeli.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
-import java.time.LocalDate;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 
 @NoArgsConstructor
@@ -11,14 +13,21 @@ import java.time.LocalDate;
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class PostDTO {
-
-    int user_id;
-    int id_post;
+    @NotNull(message = "La id no puede estar vacía")
+    @Positive(message = "El id debe ser mayor a cero")
+    Integer user_id;
+    @NotNull(message = "La id no puede estar vacía")
+    @Positive(message = "El id debe ser mayor a cero")
+    Integer id_post;
+    @NotNull(message = "La fecha no puede estar vacía.")
+    @JsonFormat(pattern = "dd-MM-yyyy")
     String  date;
+    @Valid
     ProductDTO detail;
-    int category;
-    double price;
-
-
-
+    @NotNull(message = "El campo no puede estar vacío.")
+    Integer category;
+    @NotNull(message = "El campo no puede estar vacío.")
+    @DecimalMax(value = "10000000.000", message = "El precio máximo por producto es de 10.000.000")
+    Double price;
+    
 }
