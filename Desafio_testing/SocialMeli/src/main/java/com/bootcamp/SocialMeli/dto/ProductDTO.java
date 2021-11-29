a0055b35c7405a9cc39431f6a71bb433f8650d8b
juandavidortizv <juan.ortiz@mercadolibre.com.co>
2021-11-29 08:35:36 -0500

    Validaciones agregadas a ProductoDTO.java

--- Desafio_testing/SocialMeli/src/main/java/com/bootcamp/SocialMeli/dto/ProductDTO.java
+++ Desafio_testing/SocialMeli/src/main/java/com/bootcamp/SocialMeli/dto/ProductDTO.java
@@ -3,16 +3,34 @@ package com.bootcamp.SocialMeli.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
+import javax.validation.constraints.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ProductDTO {
 
-    int product_id;
+    @NotNull(message = "La id no puede estar vacia")
+    @Positive(message = "El id debe ser mayor a 0")
+    Integer product_id;
+    @NotEmpty(message = "El campo no puede estar vacío")
+    @Size(max = 40, message = "La longitud no puede superar los 40 caracteres")
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El campo no puede poseer caracteres especiales")
     String product_name;
+    @NotEmpty(message = "El campo no puede estar vacío")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El campo no puede poseer caracteres especiales")
     String type;
+    @NotEmpty(message = "El campo no puede estar vacío")
+    @Size(max = 25, message = "La longitud no puede superar los 25 caracteres")
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El campo no puede poseer caracteres especiales")
     String brand;
+    @NotEmpty(message = "El campo no puede estar vacío")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El campo no puede poseer caracteres especiales")
     String color;
+    @Size(max = 80, message = "La longitud no puede superar los 80 caracteres")
+    @Pattern(regexp = "^[A-Z|À-Ù|Á-Ú|Ä-Ü|á-ú|ä-ü|a-z|à-ù|0-9|,|.| ]*$", message = "El campo no puede poseer caracteres especiales")
     String notes;
 }
