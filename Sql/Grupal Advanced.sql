fdfbd5b03ce0f56a4c5c2acba83667ae23e902fa
JaySMeli <jeisson.sanchez@mercadolibre.com.co>
2021-11-30 10:22:14 -0500

    Queries

--- Sql/Grupal Advanced.sql
+++ Sql/Grupal Advanced.sql
@@ -3,54 +3,110 @@
 select * from Autor;
 
 
-SElECT Nombre, edad from Estudiante;
+SElECT Nombre, edad 
+from Estudiante;
 
 
-SELECT * FROM Estudiante where carrera='Informatica';
 
 
-select * from Autor where nacionalidad in ('francesa','italiana');
+SELECT * 
+FROM Estudiante 
+where carrera='Informatica';
 
 
-select * from Libro where area <> 'Internet';
-
-select * from Libro where editorial = 'Salamanca';
 
 
-select * from Estudiante where edad > (select avg(edad) from Estudiante);
+select * 
+from Autor 
+where nacionalidad in ('francesa','italiana');
 
-select nombre as nombre_estudiante from Estudiante where nombre like 'G%';
+
+select * 
+from Libro 
+where area <> 'Internet';
+
+
+
+select * 
+from Libro 
+where editorial = 'Salamanca';
+
+
+
+
+
+select * 
+from Estudiante 
+where edad > (select avg(edad) from Estudiante);
+
+
+
+
+select nombre as nombre_estudiante 
+from Estudiante where nombre like 'G%';
+
+
+
 
 select au.* 
 from Libro l, LibroAutor a, Autor au 
-where l.titulo = 'El Universo: Guía de viaje' and au.idAutor = a.idAutor and l.idLibro=a.idLibro;
+where 
+	l.titulo = 'El Universo: Guía de viaje' 
+    and au.idAutor = a.idAutor and l.idLibro=a.idLibro;
+
+
+
 
 select *
 from Autor
 where nacionalidad in ('argentina','italiana');
 
 
+
+
 select l.*
-from Libro l inner join Prestamo p on l.idLibro=p.idLibro inner join Estudiante e on e.idLector=p.idLector
+from 
+	Libro l inner join Prestamo p on l.idLibro=p.idLibro 
+	inner join Estudiante e on e.idLector=p.idLector
 WHERE e.nombre='Filippo Galli';
+
+
+
 
 select *
 from estudiante
 where edad = (select min(edad) from estudiante);
 
+
+
 select e.*
-from Libro l inner join Prestamo p on l.idLibro=p.idLibro inner join Estudiante e on e.idLector=p.idLector
+from 
+	Libro l inner join Prestamo p on l.idLibro=p.idLibro 
+    inner join Estudiante e on e.idLector=p.idLector
 WHERE l.titulo like '%Base de Datos%';
+
+
+
 
 select l.* 
 from Libro l, LibroAutor a, Autor au 
-where au.nombre = 'J.K. Rowling' and au.idAutor = a.idAutor and l.idLibro=a.idLibro;
+where 
+		au.nombre = 'J.K. Rowling' 
+		and au.idAutor = a.idAutor 
+        and l.idLibro=a.idLibro;
+
+
 
 select l.titulo
 from Libro l inner join Prestamo p on p.idLibro = l.idLibro
 where p.FechaDevolución = '2021-07-16';
 
-select * from Estudiante where edad > (select avg(edad) from Estudiante);
+
+
+
+select * 
+from Estudiante 
+where edad > (select avg(edad) from Estudiante);
 
 
 
