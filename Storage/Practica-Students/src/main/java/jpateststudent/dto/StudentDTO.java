26dc811aa43776d17122e2c890f353281dfe204e
anaherreraMeLi <analia.herrera@mercadolibre.com>
2021-12-03 14:13:19 -0300

    Paractica en grupo

---
+++ Storage/Practica-Students/src/main/java/jpateststudent/dto/StudentDTO.java
@@ -0,0 +1,13 @@
+package jpateststudent.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StudentDTO {
+    private Long id;
+    private String dni;
+    private String name;
+    private String lastName;
+}
