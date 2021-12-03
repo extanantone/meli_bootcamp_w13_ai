028fc4acd6659575549c63afbf9c579401f57599
Birustia1 <brian.irustia@mercadolibre.com>
2021-12-03 15:12:48 -0300

    Punto 1 con solucion error last name

--- Storage/ProbandoJPA/src/main/java/com/meli/probandojpa/dto/StudentDTO.java
+++ Storage/ProbandoJPA/src/main/java/com/meli/probandojpa/dto/StudentDTO.java
@@ -1,10 +1,13 @@
 package com.meli.probandojpa.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class StudentDTO {
 
     private Long id;
