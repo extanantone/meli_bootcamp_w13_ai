149697f12f1adb15f9b01740b7be1f6a5418d41b
JaySMeli <jeisson.sanchez@mercadolibre.com.co>
2021-12-06 13:12:30 -0500

    app

--- Spring JPA/movies/src/main/java/com/movies/model/Actor.java
+++ Spring JPA/movies/src/main/java/com/movies/model/Actor.java
@@ -1,12 +1,9 @@
 package com.movies.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonIncludeProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import javax.persistence.*;
 import java.time.LocalDate;
 
