39d50073d18bade26b40281437989fe058cc2eed
Tomás Wagner <tomas-wagner@mercadolibre.com>
2021-12-14 16:13:38 -0300

    refactor: mueve lógica de negocios a servicio

--- Quality/NumerosRomanos/src/main/java/com/bootcamp/demo/demo/RomanNumbersApplication.java
+++ Quality/NumerosRomanos/src/main/java/com/bootcamp/demo/demo/RomanNumbersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NumerosRomanosApplication {
+public class RomanNumbersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NumerosRomanosApplication.class, args);
+		SpringApplication.run(RomanNumbersApplication.class, args);
 	}
 
 }
--- Quality/NumerosRomanos/src/main/java/com/bootcamp/demo/demo/controller/RomanNumbersController.java
+++ Quality/NumerosRomanos/src/main/java/com/bootcamp/demo/demo/controller/RomanNumbersController.java
@@ -1,15 +1,15 @@
 package com.bootcamp.demo.demo.controller;
 
-import com.bootcamp.demo.demo.model.NumeroDecimal;
+import com.bootcamp.demo.demo.service.RomanNumbersService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class NumerosRomanosController {
+public class RomanNumbersController {
     @GetMapping("/{num}")
-    public static String decimalARomano(@PathVariable Integer num){
-        NumeroDecimal numeroDecimal = new NumeroDecimal(num);
-        return numeroDecimal.aRomano();
+    public static String romanFromDecimal(@PathVariable Integer num){
+        //TODO: input validation
+        return RomanNumbersService.getRomanFromDecimal(num);
     }
 }
